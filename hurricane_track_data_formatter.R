a8a400ef5433cc13914ba4ebebaca852c398cd26
connermcb <connermcb@yahoo.com>
2017-11-18 02:28:51 -0600

    data transform code to function, get possible coords function

--- hurricane_track_data_formatter.R
+++ hurricane_track_data_formatter.R
@@ -10,71 +10,82 @@ library(png)
 library(reshape)
 library(tidyr)
 
-## load data file
-# file name and set working directory
 f <- "ebtrk_atlc_1988_2015.txt"
-setwd("c:/users/conner/adv_programming_R")
+wd <- "c:/users/conner/adv_programming_R"
 
-# read table from text file and set column names
-ext_tracks_widths <- c(7, 10, 2, 2, 3, 5, 5, 6, 4, 5, 4, 4, 5, 3, 4, 3, 3, 3,
-                       4, 3, 3, 3, 4, 3, 3, 3, 2, 6, 1)
 
-ext_tracks_colnames <- c("storm_id", "storm_name", "month", "day",
-                         "hour", "year", "latitude", "longitude",
-                         "max_wind", "min_pressure", "rad_max_wind",
-                         "eye_diameter", "pressure_1", "pressure_2",
-                         paste("radius_34", c("ne", "se", "sw", "nw"), sep = "_"),
-                         paste("radius_50", c("ne", "se", "sw", "nw"), sep = "_"),
-                         paste("radius_64", c("ne", "se", "sw", "nw"), sep = "_"),
-                         "storm_type", "distance_to_land", "final")
+get_hrcn_data <- function(f, wd, hrcn, yr){
+  ## load data file
+  # file name and set working directory
+  setwd(wd)
 
-# read file
-hrcns <- read.fwf(f, widths = ext_tracks_widths, col.names = ext_tracks_colnames)
-
-## Tidy, clean, and subset
-# reformat `storm_id` variable combining name and year
-hrcns["storm_id"] = paste(sub("\\s+$", "", hrcns$storm_name), hrcns$year, sep="-")
-
-# combine time and date variables
-hrcns["date_time"] <- as_datetime(
-  paste(
-    paste(hrcns$year, hrcns$month, hrcns$day, sep="/"), 
-    paste(hrcns$hour, c(rep("00", 2)), sep = ":")
+  # format hurricane, year variable as "[hurricane_name]-[year]"
+  hy <- paste0(as.character(hrcn), "-", as.character(yr))
+  # read table from text file and set column names
+  ext_tracks_widths <- c(7, 10, 2, 2, 3, 5, 5, 6, 4, 5, 4, 4, 5, 3, 4, 3, 3, 3,
+                         4, 3, 3, 3, 4, 3, 3, 3, 2, 6, 1)
+  
+  ext_tracks_colnames <- c("storm_id", "storm_name", "month", "day",
+                           "hour", "year", "latitude", "longitude",
+                           "max_wind", "min_pressure", "rad_max_wind",
+                           "eye_diameter", "pressure_1", "pressure_2",
+                           paste("radius_34", c("ne", "se", "sw", "nw"), sep = "_"),
+                           paste("radius_50", c("ne", "se", "sw", "nw"), sep = "_"),
+                           paste("radius_64", c("ne", "se", "sw", "nw"), sep = "_"),
+                           "storm_type", "distance_to_land", "final")
+  
+  # read file
+  hrcns <- read.fwf(f, widths = ext_tracks_widths, col.names = ext_tracks_colnames)
+  
+  ## Tidy, clean, and subset
+  # reformat `storm_id` variable combining name and year
+  hrcns["storm_id"] = paste(sub("\\s+$", "", hrcns$storm_name), hrcns$year, sep="-")
+  
+  # combine time and date variables
+  hrcns["date_time"] <- as_datetime(
+    paste(
+      paste(hrcns$year, hrcns$month, hrcns$day, sep="/"), 
+      paste(hrcns$hour, c(rep("00", 2)), sep = ":")
+      )
     )
-)
+  
+  # subset 
+  subset_vars <- c("storm_id", "date_time", "latitude", "longitude", 
+                   paste("radius_34", c("ne", "se", "sw", "nw"), sep = "_"),
+                   paste("radius_50", c("ne", "se", "sw", "nw"), sep = "_"),
+                   paste("radius_64", c("ne", "se", "sw", "nw"), sep = "_"))
+  
+  hrcns <- hrcns[subset_vars]
+                          
+  # long form
+  hrcns <- hrcns%>%
+    gather(txt_rd, dist, radius_34_ne:radius_64_nw, na.rm=TRUE)
+  
+  
+  hrcns <- hrcns%>%
+    separate(txt_rd, c("txt", "wind_speed", "wind_direction"), sep="_")
+  
+  hrcns <- hrcns[, -5]
+  
+  hrcns <- hrcns%>%
+    spread(wind_direction, dist)%>%
+    arrange(storm_id, date_time)
+  
+  hrcns["wind_speed"] <- as.integer(hrcns$wind_speed)
+  
+  hrcns["longitude"] <- hrcns["longitude"] * -1
 
-# subset 
-subset_vars <- c("storm_id", "date_time", "latitude", "longitude", 
-                 paste("radius_34", c("ne", "se", "sw", "nw"), sep = "_"),
-                 paste("radius_50", c("ne", "se", "sw", "nw"), sep = "_"),
-                 paste("radius_64", c("ne", "se", "sw", "nw"), sep = "_"))
-
-hrcns <- hrcns[subset_vars]
-                        
-# long form
-hrcns <- hrcns%>%
-  gather(txt_rd, dist, radius_34_ne:radius_64_nw, na.rm=TRUE)
-
-
-hrcns <- hrcns%>%
-  separate(txt_rd, c("txt", "wind_speed", "wind_direction"), sep="_")
-
-hrcns <- hrcns[, -5]
-
-hrcns <- hrcns%>%
-  spread(wind_direction, dist)%>%
-  arrange(storm_id, date_time)
-
-hrcns["wind_speed"] <- as.integer(hrcns$wind_speed)
-
-hrcns["longitude"] <- hrcns["longitude"] * -1
-
-head(hrcns)
-
-ike_2008 <- hrcns[hrcns$storm_id=="IKE-2008",]
-table(ike_2008$latitude)
+  hrcn_data <- hrcns[hrcns$storm_id==hy,]
+  return(hrcn_data)
+}
+  
+list_coords <- function(h_data){
+  return(h_data)[,c("latitude", "longitude")]
+}
+  
+  
 ike2008 <- ike_2008[ike_2008$latitude == 26.9,]
-
+ike2008
 
 
 test <- melt(test[3:9], 
